7a871c8b4b36f382b821e0f43bca088fc20a9c38
Shiuan Chou <im.cps0903@gmail.com>
2023-03-31 16:56:32 +0800

    create-init project

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="zh-hant">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
