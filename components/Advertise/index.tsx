f55a4456a83343bf826c416eb14898be1d74be22
Shiuan Chou <im.cps0903@gmail.com>
2023-04-02 23:42:15 +0800

    create-advertise section

---
+++ components/Advertise/index.tsx
@@ -0,0 +1,11 @@
+import Footer from '@components/Footer'
+import styles from './_Advertise.module.scss'
+
+export default function Advertise() {
+    return (
+        <aside className={styles.container}>
+            <h2>專為今天和明天的性平教育而設計</h2>
+            <h3>Sub Title</h3>
+        </aside>
+    )
+}
