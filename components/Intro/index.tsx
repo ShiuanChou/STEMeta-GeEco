d917058f96cbc92634c264c8a26a8459221ba765
Shiuan Chou <im.cps0903@gmail.com>
2023-04-03 21:39:48 +0800

    modify-no-unescaped-entities error

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
         <section className={styles.container} id="intro">
             <div className={`${styles.wrap} ${styles.introWord}`}>
                 <h2>Female Scientist</h2>
-                <p>If you're in need of some creative inspiration, using nonsense words can be a great way to get the creative juices flowing. Simply generate a fake word and then come up with a definition of what that word could mean. Since it's not a real word, there's no right or wrong answer. You can let your imagination go wild and that should help to get the creative juices flowing. It's a great way to use a made up word generator.</p>
+                <p>If youre in need of some creative inspiration, using nonsense words can be a great way to get the creative juices flowing. Simply generate a fake word and then come up with a definition of what that word could mean. Since its not a real word, theres no right or wrong answer. You can let your imagination go wild and that should help to get the creative juices flowing. Its a great way to use a made up word generator.</p>
             </div>
             <div className={`${styles.wrap} ${styles.person}`}>
                 <Image src={person} alt='女性科學家' />
