f67dd7e90cf3da22575549a058c724b8cad27920
Shiuan Chou <im.cps0903@gmail.com>
2023-04-03 15:29:50 +0800

    modify-more botton scroll to intro effect

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -4,7 +4,7 @@ import person from '/public/images/person.jpg';
 
 export default function Intro() {
     return (
-        <section className={styles.container}>
+        <section className={styles.container} id="intro">
             <div className={`${styles.wrap} ${styles.introWord}`}>
                 <h2>Female Scientist</h2>
                 <p>If you're in need of some creative inspiration, using nonsense words can be a great way to get the creative juices flowing. Simply generate a fake word and then come up with a definition of what that word could mean. Since it's not a real word, there's no right or wrong answer. You can let your imagination go wild and that should help to get the creative juices flowing. It's a great way to use a made up word generator.</p>
--- components/VideoBanner/index.tsx
+++ components/VideoBanner/index.tsx
@@ -2,6 +2,15 @@ import styles from './_VideoBanner.module.scss';
 import { FiArrowDown } from 'react-icons/fi'
 
 export default function VideoBanner() {
+    const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+        const elem = document.getElementById('intro');
+        window.scrollTo({
+            top: elem?.getBoundingClientRect().top,
+            behavior: "smooth"
+        })
+    }
+
     return (
         <div className={styles.container}>
             <video className={styles.video} autoPlay muted loop>
@@ -10,7 +19,7 @@ export default function VideoBanner() {
             <section className={styles.title}>
                 <h1>STEMate & GeEco</h1>
                 <p>助推女性跨入STEM領域</p>
-                <div className={styles.more}>
+                <div className={styles.more} onClick={handleScroll}>
                     <span><FiArrowDown /></span>
                     <p>開始探索</p>
                 </div>
