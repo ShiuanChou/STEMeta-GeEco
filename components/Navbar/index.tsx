9b6dfd0293cabf6f774102ded1187a6871e69b4a
Shiuan Chou <im.cps0903@gmail.com>
2023-04-03 15:27:53 +0800

    modify-navbar rwd(1024px) styles, li hover styles

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,25 @@
+import Image from 'next/image'
 import styles from './_Navbar.module.scss'
+import logo from '/public/images/logo.png'
+import Link from 'next/link'
+import { FiMenu } from 'react-icons/fi'
 
 export default function Navbar() {
     return (
         <nav className={styles.container}>
-            <div>LOGO</div>
+            <Link href={'/'} className={styles.logo}>
+                <Image src={logo} alt="logo" />
+            </Link>
             <ul className={styles.link}>
                 <li>元宇宙</li>
                 <li>NFT展示</li>
                 <li>證書存放</li>
                 <li>課程簡介</li>
             </ul>
-            <div>Login</div>
+            <span className={styles.menu}>
+                <FiMenu />
+            </span>
+            {/* <div>Login</div> */}
         </nav>
     )
 }
