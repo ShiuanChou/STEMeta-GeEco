20523a1d95389f8dbb36fb125861e3da4544a27a
Shiuan Chou <im.cps0903@gmail.com>
2023-04-03 22:59:12 +0800

    modify-heiglight color change, title word fix

--- components/VideoBanner/index.tsx
+++ components/VideoBanner/index.tsx
@@ -2,7 +2,7 @@ import styles from './_VideoBanner.module.scss';
 import { FiArrowDown } from 'react-icons/fi'
 
 export default function VideoBanner() {
-    const handleScroll = (e: React.MouseEvent<Element, MouseEvent>) : void => {
+    const handleScroll = (e: React.MouseEvent<Element, MouseEvent>): void => {
         e.preventDefault();
         const elem = document.getElementById('intro');
         window.scrollTo({
@@ -17,8 +17,8 @@ export default function VideoBanner() {
                 <source src="/video/background.mp4" type="video/mp4" />
             </video>
             <section className={styles.title}>
-                <h1>STEMate & GeEco</h1>
-                <p>助推女性跨入STEM領域</p>
+                <h1>STEMeta & GeEco</h1>
+                <p>"元"創教育{' | '}女力行<br />助推女性跨入STEM領域</p>
                 <div className={styles.more} onClick={handleScroll}>
                     <span><FiArrowDown /></span>
                     <p>開始探索</p>
