b4b28dedacd951d044fee9ee5e721bc960826bd7
Shiuan Chou <im.cps0903@gmail.com>
2023-04-05 09:22:41 +0800

    modify-intro content and style

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
 import styles from './_Intro.module.scss';
-import person from '/public/images/person.jpg';
+import person from '/public/images/metaverse.png';
+import { FiSearch, FiPlay } from 'react-icons/fi';
 
 export default function Intro() {
     return (
         <section className={styles.container} id="intro">
             <div className={`${styles.wrap} ${styles.introWord}`}>
-                <h2>Female Scientist</h2>
-                <p>If youre in need of some creative inspiration, using nonsense words can be a great way to get the creative juices flowing. Simply generate a fake word and then come up with a definition of what that word could mean. Since its not a real word, theres no right or wrong answer. You can let your imagination go wild and that should help to get the creative juices flowing. Its a great way to use a made up word generator.</p>
+                <h2>快來建造專屬你的</h2>
+                <h2 className={styles.highlight}>性平&quot;元&quot;城市</h2>
+                <p>模擬城市-自己當市長<br />好玩性、可靠性和創意性<br />盡在P2L City</p>
+                <div className={styles.btnGroup}>
+                    <button className={styles.btnVisit}><span><FiSearch /></span>Visit Now</button>
+                    <button className={styles.btnPlay}><span><FiPlay /></span>Watch Video</button>
+                </div>
             </div>
             <div className={`${styles.wrap} ${styles.person}`}>
-                <Image src={person} alt='女性科學家' />
+                <Image src={person} alt='metaverse' />
             </div>
         </section>
     )
